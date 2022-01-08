78007778c04975b3e2bee588316dcdc99c6e4174
remittor <remittor@gmail.com>
2022-01-08 16:45:37 +0300

    Change default temp and fan speed values

--- package/luci-app-alpine-fan-control/luasrc/model/cbi/alpine-fan-control.lua
+++ package/luci-app-alpine-fan-control/luasrc/model/cbi/alpine-fan-control.lua
@@ -40,37 +40,37 @@ d.rmempty  = false
 
 min_temp = s:option(Value, "min_temp", translate("min_temp"), translate("Temperature for minimum fan state (Celsius)"))
     min_temp.datatype = "uinteger"
-    min_temp.default = "30"
+    min_temp.default = "36"
     min_temp.rmempty = "false"
     min_temp.optional = "false"
 
 min_speed = s:option(Value, "min_speed", translate("min_speed"), translate("Fan speed at minimum fan state"))
     min_speed.datatype = "uinteger"
-    min_speed.default = "0"
+    min_speed.default = "785"
     min_speed.rmempty = "false"
     min_speed.optional = "false"
 
 mid_temp = s:option(Value, "mid_temp", translate("mid_temp"), translate("Temperature for medium fan state (Celsius)"))
     mid_temp.datatype = "uinteger"
-    mid_temp.default = "50"
+    mid_temp.default = "41"
     mid_temp.rmempty = "false"
     mid_temp.optional = "false"
 
 mid_speed = s:option(Value, "mid_speed", translate("mid_speed"), translate("Fan speed at medium fan state"))
     mid_speed.datatype = "uinteger"
-    mid_speed.default = "2500"
+    mid_speed.default = "2400"
     mid_speed.rmempty = "false"
     mid_speed.optional = "false"
 
 max_temp = s:option(Value, "max_temp", translate("max_temp"), translate("Temperature for maximum fan state (Celsius)"))
     max_temp.datatype = "uinteger"
-    max_temp.default = "65"
+    max_temp.default = "50"
     max_temp.rmempty = "false"
     max_temp.optional = "false"
 
 max_speed = s:option(Value, "max_speed", translate("max_speed"), translate("Fan speed at minimum fan state - can be set to 0"))
     max_speed.datatype = "uinteger"
-    max_speed.default="4000"
+    max_speed.default="2500"
     max_speed.rmempty="false"
     max_speed.optional="false"
 
