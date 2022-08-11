7665bb2010dd8d2521fcf5a960e9be2a1e214e2c
Oleg S <remittor@gmail.com>
2022-08-11 20:30:15 +0300

    Set menu order to 58

--- package/luci-app-alpine-fan-control/luasrc/controller/alpine-fan-control.lua
+++ package/luci-app-alpine-fan-control/luasrc/controller/alpine-fan-control.lua
@@ -3,7 +3,7 @@ module("luci.controller.alpine-fan-control", package.seeall)
 function index()
 	if nixio.fs.access("/etc/config/alpine-fan-control") then
 		local page
-		page = entry({"admin", "system", "alpine-fan-control"}, cbi("alpine-fan-control"), _("Fan Control"))
+		page = entry({"admin", "system", "alpine-fan-control"}, cbi("alpine-fan-control"), _("Fan Control"), 58)
 		page.dependent = true
 	end
 end
